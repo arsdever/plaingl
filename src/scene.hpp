04b4aa9d4229815375e905941e6ee3dbaa4300b2
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2023-12-02 12:17:06 +0400

    Add simple scene implementation

---
+++ src/scene.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+// TODO: the implementation is very draft and needs redoing
+
+class game_object;
+
+class scene
+{
+public:
+    scene();
+
+    const std::vector<game_object*>& objects() const;
+
+    void add_object(game_object* object);
+
+    static scene& get_active_scene();
+
+private:
+    std::vector<game_object*> _objects;
+    static scene* _scene_instance;
+};
