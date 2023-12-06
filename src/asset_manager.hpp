6ed727c4fd90f3739a92e799273daa4a27967bf1
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2023-12-06 18:04:25 +0400

    Make asset_manager store objects in maps to their filenames

--- src/asset_manager.hpp
+++ src/asset_manager.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <string>
 #include <string_view>
-#include <vector>
+#include <unordered_map>
+
+#include "utils.hpp"
 
 class mesh;
 class material;
@@ -10,20 +13,26 @@ class shader_program;
 
 class asset_manager
 {
-public:
+private:
     asset_manager() = default;
 
+public:
     void load_asset(std::string_view path);
-    const std::vector<mesh*>& meshes() const;
-    const std::vector<material*>& materials() const;
-    const std::vector<image*>& textures() const;
+    const std::vector<mesh*> meshes() const;
+    const std::vector<material*> materials() const;
+    const std::vector<image*> textures() const;
+    const std::vector<shader_program*> shaders() const;
 
     static asset_manager* default_asset_manager();
 
 private:
-    std::vector<mesh*> _meshes;
-    std::vector<material*> _materials;
-    std::vector<image*> _textures;
-    std::vector<shader_program*> _shaders;
+    template <typename T>
+    using asset_map =
+        std::unordered_map<std::string, T, string_hash, std::equal_to<>>;
+
+    asset_map<std::vector<mesh*>> _meshes;
+    asset_map<material*> _materials;
+    asset_map<image*> _textures;
+    asset_map<shader_program*> _shaders;
     static asset_manager* _instance;
 };
