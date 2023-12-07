430e29796a0057c2bacc581ca80e2fa747886499
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2023-12-08 03:01:39 +0400

    Add shader getter and 3d line drawing in gizmo_drawer

--- src/gizmo_drawer.cpp
+++ src/gizmo_drawer.cpp
@@ -15,9 +15,7 @@ void gizmo_drawer::init()
     shader_program::unuse();
 }
 
-void gizmo_drawer::draw_line(glm::vec3, glm::vec3) { }
-
-void gizmo_drawer::draw_line_2d(glm::vec2 p1, glm::vec2 p2, glm::vec4 color)
+void gizmo_drawer::draw_line(glm::vec3 p1, glm::vec3 p2, glm::vec4 color)
 {
     _gizmo_shader.use();
     unsigned vao;
@@ -26,9 +24,14 @@ void gizmo_drawer::draw_line_2d(glm::vec2 p1, glm::vec2 p2, glm::vec4 color)
     glBindVertexArray(vao);
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    std::array<glm::vec3, 2> verts { { { p1, 0 }, { p2, 0 } } };
+    std::array<glm::vec3, 2> verts { { p1, p2 } };
     glBufferData(GL_ARRAY_BUFFER,
-                 verts.size() * sizeof(glm::vec2::type) * 3,
+                 // number of vertices
+                 verts.size() *
+                     // glm::vec component's type size
+                     sizeof(decltype(verts)::value_type::value_type) *
+                     // glm::vec component count
+                     sizeof(decltype(verts)::value_type::length()),
                  verts.data(),
                  GL_STATIC_DRAW);
 
@@ -42,6 +45,39 @@ void gizmo_drawer::draw_line_2d(glm::vec2 p1, glm::vec2 p2, glm::vec4 color)
     glDeleteVertexArrays(1, &vao);
     shader_program::unuse();
 }
+
+void gizmo_drawer::draw_line_2d(glm::vec2 p1, glm::vec2 p2, glm::vec4 color)
+{
+    _gizmo_shader.use();
+    unsigned vao;
+    unsigned vbo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    std::array<glm::vec3, 2> verts { { { p1, 0 }, { p2, 0 } } };
+    glBufferData(GL_ARRAY_BUFFER,
+                 // number of vertices
+                 verts.size() *
+                     // glm::vec component's type size
+                     sizeof(decltype(verts)::value_type::value_type) *
+                     // glm::vec component count
+                     sizeof(decltype(verts)::value_type::length()),
+                 verts.data(),
+                 GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glUniform4fv(_color_location, 1, glm::value_ptr(color));
+    glDrawArrays(GL_LINES, 0, 2);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glDeleteBuffers(1, &vbo);
+    glBindVertexArray(0);
+    glDeleteVertexArrays(1, &vao);
+    shader_program::unuse();
+}
+
+shader_program& gizmo_drawer::get_shader() { return _gizmo_shader; }
 
 gizmo_drawer* gizmo_drawer::instance()
 {
--- src/gizmo_drawer.hpp
+++ src/gizmo_drawer.hpp
@@ -10,8 +10,10 @@ class gizmo_drawer
 public:
     void init();
 
-    void draw_line(glm::vec3 p1, glm::vec3 p2);
+    void draw_line(glm::vec3 p1, glm::vec3 p2, glm::vec4 color);
     void draw_line_2d(glm::vec2 p1, glm::vec2 p2, glm::vec4 color);
+
+    shader_program& get_shader();
 
     static gizmo_drawer* instance();
 
