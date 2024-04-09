55971ab8a7aa394e9a0191bc6f98008ff92a3402
Arsen <arsen.gharagyozyn.96@gmail.com>
2024-04-09 12:29:21 +0400

    Improve watch mechanism for files (#25)
    * Introduce file class for file manipulations
    
    * Implement missing file operations
    
    * Implement missing constructors and move get_file_contents into file
    
    * Move exists into file
    
    * Implement basic filewatch integration
    
    * Improve filewatch library cmake integration
    
    * Improve file interface and add changed event
    
    * Add file watching hook mechanism
    
    * Cleanup file.cpp

--- src/file.hpp
+++ src/file.hpp
@@ -25,6 +25,24 @@ public:
         unknown,
     };
 
+    class file_watch_hook
+    {
+    private:
+        struct pdata;
+
+    public:
+        file_watch_hook(pdata&&);
+        file_watch_hook& operator=(file_watch_hook&&);
+        file_watch_hook(file_watch_hook&&);
+        file_watch_hook() = delete;
+        file_watch_hook(const file_watch_hook&) = delete;
+        file_watch_hook& operator=(const file_watch_hook&) = delete;
+        ~file_watch_hook();
+
+    private:
+        std::unique_ptr<pdata> _p;
+    };
+
 public:
     file(std::string path);
     file(const file&) = delete;
@@ -44,7 +62,9 @@ public:
 
     event<void(event_type)> changed_externally;
 
-    static void watch(std::string_view path);
+    static file_watch_hook
+    watch(std::string_view path,
+          std::function<void(std::string_view, event_type)> functor);
     static event<void(std::string_view)> generic_file_change;
 
     static std::string read_all(std::string_view path);
