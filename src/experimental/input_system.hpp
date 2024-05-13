5f095665600d3ad7bb33d92969e9f7057e210408
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2024-05-14 01:45:17 +0400

    Add keypress event in experimental::input_system

--- src/experimental/input_system.hpp
+++ src/experimental/input_system.hpp
@@ -5,6 +5,8 @@
 
 #include <glm/fwd.hpp>
 
+#include "event.hpp"
+
 namespace experimental
 {
 
@@ -43,6 +45,8 @@ public:
 
     static bool is_key_down(int keycode);
     static void set_key_down(int keycode, bool state = true);
+
+    static event<void(int)> on_keypress;
 
 private:
     static std::unordered_set<int> _pressed_keys;
