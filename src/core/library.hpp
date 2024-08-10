9e33e1352ba51c011013e8c277905656016d8064
Arsen <arsen.gharagyozyn.96@gmail.com>
2024-08-10 10:08:09 +0400

    Add library class (#47)

---
+++ src/core/library.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "common/utils.hpp"
+
+namespace core
+{
+class library
+{
+public:
+    library(std::string path);
+    library(const library&) = delete;
+    library& operator=(const library&) = delete;
+    library(library&&);
+    library& operator=(library&&);
+    ~library();
+
+    void lazy_load();
+    void release();
+
+    template <typename T>
+    deduce_function_type<T>::type resolve(std::string_view name)
+    {
+        auto* const proc_address = resolve(name);
+        return reinterpret_cast<deduce_function_type<T>::type>(proc_address);
+    }
+
+private:
+    using ftype = long long (*)();
+    ftype resolve(std::string_view name);
+
+private:
+    struct impl;
+    std::unique_ptr<impl> _impl;
+};
+} // namespace core
