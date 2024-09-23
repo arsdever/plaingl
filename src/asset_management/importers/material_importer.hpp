9cbaf6c6038a543202af5a4434d21893b89c987d
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2024-09-23 18:26:50 +0400

    Implement material reloading

--- src/asset_management/importers/material_importer.hpp
+++ src/asset_management/importers/material_importer.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "asset_management/assets_fwd.hpp"
+
 #include "asset_management/type_importer.hpp"
 
 class material_importer : public assets::type_importer<graphics::material>
@@ -7,4 +9,5 @@ class material_importer : public assets::type_importer<graphics::material>
 public:
     static constexpr std::string_view extensions { ".mat" };
     void internal_load(common::file& asset_file) override;
+    void internal_update(asset_data_t mat, common::file& asset_file) override;
 };
