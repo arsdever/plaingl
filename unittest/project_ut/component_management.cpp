fb8b23ece4524d9c622bd21de835242f3b5473d7
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2024-12-29 18:34:02 +0400

    Fix propagation of the gameobject messages to the nested objects

--- unittest/project_ut/component_management.cpp
+++ unittest/project_ut/component_management.cpp
@@ -1,5 +1,7 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "project/component_interface/component_registry.hpp"
 #include "project/components/mesh_filter.hpp"
 #include "project/components/mesh_renderer.hpp"
 #include "project/game_object.hpp"
@@ -35,10 +37,51 @@ TEST(ProjectComponentManagement, add_component_same_as_get_component_2)
 
     auto obj = game_object::create();
     std::shared_ptr<graphics::mesh> m = std::shared_ptr<graphics::mesh>(
-        reinterpret_cast<graphics::mesh*>(0xff00ff00ff00ff00), [](auto) {});
+        reinterpret_cast<graphics::mesh*>(0xff00ff00ff00ff00), [](auto) { });
     obj->add<components::mesh_filter>().set_mesh(m);
     auto& mf = obj->get<components::mesh_filter>();
 
     EXPECT_EQ(mf.get_mesh().get(),
               reinterpret_cast<graphics::mesh*>(0xff00ff00ff00ff00));
+}
+
+class test_component : public component
+{
+public:
+    using base = component;
+
+    test_component(game_object& obj)
+        : component(type_name, obj)
+    {
+    }
+
+    MOCK_METHOD(void, on_init, (), (override));
+    MOCK_METHOD(void, on_update, (), (override));
+    MOCK_METHOD(void, on_deinit, (), (override));
+
+    static constexpr std::string_view type_name = "test_component";
+};
+
+TEST(ProjectComponentManagement, nested_object_component_update)
+{
+    project_manager::initialize();
+    component_registry::register_component<test_component>("test_component");
+
+    auto obj = game_object::create();
+
+    auto nested = game_object::create();
+    obj->add_child(nested);
+
+    auto& tc = nested->add<test_component>();
+
+    EXPECT_CALL(tc, on_init()).Times(1);
+
+    EXPECT_CALL(tc, on_update()).Times(5);
+
+    obj->init();
+
+    for (int i = 0; i < 5; i++)
+    {
+        obj->update();
+    }
 }
