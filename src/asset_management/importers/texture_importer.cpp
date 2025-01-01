bcf2d1aa7b7742806d31a73898ec7c4f373765ec
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2025-01-01 04:20:02 +0400

    Implement texture updating

--- src/asset_management/importers/texture_importer.cpp
+++ src/asset_management/importers/texture_importer.cpp
@@ -1,8 +1,15 @@
 #include "asset_management/importers/texture_importer.hpp"
 
+#include "common/main_thread_dispatcher.hpp"
 #include "graphics/texture.hpp"
 
 void texture_importer::internal_load(common::file& asset_file)
 {
     _data = graphics::texture::from_file(asset_file);
+}
+
+void texture_importer::internal_update(std::shared_ptr<graphics::texture> txt,
+                                       common::file& f)
+{
+    txt->set_contents_from_file(f);
 }
--- src/asset_management/importers/texture_importer.hpp
+++ src/asset_management/importers/texture_importer.hpp
@@ -7,4 +7,6 @@ class texture_importer : public assets::type_importer<graphics::texture>
 public:
     static constexpr std::string_view extensions { ".png,.jpg,.jpeg" };
     void internal_load(common::file& asset_file) override;
+
+    void internal_update(std::shared_ptr<graphics::texture>, common::file&) override;
 };
